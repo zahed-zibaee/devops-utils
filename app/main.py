6d221c469cde6f7453d2c54dc0208e1156132d86
Amirreza Hosseini <amirreza.hosseini@snapp.supply>
2024-03-04 13:16:02 +0330

    fix https url

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from .routers import lock
-
+import os
 
 app = FastAPI()
 
@@ -11,18 +11,13 @@ app.include_router(lock.router)
 
 templates = Jinja2Templates(directory="app/templates")
 
-def https_url_for(request: Request, name: str, **path_params) -> str:
-
-    http_url = request.url_for(name, **path_params)
-
-    # Replace 'http' with 'https'
-    return http_url.replace("http", "https", 1)
-
-templates.env.globals["https_url_for"] = https_url_for
-
-
+#base_url = os.getenv("BASE_URL")
+base_url = "utils.snapp.supply"
 
 @app.get("/", response_class=HTMLResponse)
 async def form(request: Request):
     locked_time = await lock.locked_days()
-    return templates.TemplateResponse(name="index.html", context={"request": request, "locked_time": locked_time})
+    return templates.TemplateResponse(name="index.html", context={
+                                        "request": request, 
+                                        "locked_time": locked_time, 
+                                        "base_url": base_url})
