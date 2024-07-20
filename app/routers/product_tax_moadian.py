a3b787984a74a789dbb5c86bef3de3a22a906ab4
Zahed zibaee <zahed.zibaee@snapp.supply>
2024-07-20 15:51:22 +0000

    fix

--- app/routers/product_tax_moadian.py
+++ app/routers/product_tax_moadian.py
@@ -146,10 +146,9 @@ async def update_products(
                     raise HTTPException(status_code=404, detail="Can not get product {_id} from database".format(_id=row["id"]))
             product.tax_rate = row["tax_rate"] 
             product.moadian_product_id = row["moadian_product_id"]
-            updated_products.append(product)     
-            return updated_products
-
-                
+            updated_products.append(product)
+        return updated_products
+        
     for chunk in pd.read_csv(file.file, chunksize=1000, iterator=True):
         if "ID" not in chunk.columns or "Tax Rate" not in chunk.columns or "Moadian Product ID" not in chunk.columns :
             raise HTTPException(
